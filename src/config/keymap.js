4885924ecb3d582a864b2eee74dac88b97f07131
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-07-06 00:58:14 +0200

    :trollface: Version: 0.1.0

---
+++ src/config/keymap.js
@@ -0,0 +1,10 @@
+
+const keys = [{
+  name: 'New File',
+  description: 'Press ctrl+n to create a new file',
+  map: 'ctrl+n'
+}, {
+  name: 'Open File',
+  description: 'Press ctrl+o to open a file',
+  map: 'ctrl+o'
+}]
