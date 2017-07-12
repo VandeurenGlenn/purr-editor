8342fc28e1746cc764a22cb5ca65f25d37297902
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-07-13 00:58:31 +0200

    :metal: AutoSave, snippets, autoCompletion, etc ...

---
+++ _build/copy.sh
@@ -0,0 +1,10 @@
+PRE_DEST='build/bundled/bower_components'
+
+mkdir -p $PRE_DEST/ace-builds
+cp -u -f -R bower_components/ace-builds/src-min-noconflict $PRE_DEST/ace-builds
+
+mkdir -p $PRE_DEST/jszip/dist
+cp -u -f -R bower_components/jszip/dist/*min.js $PRE_DEST/jszip/dist/jszip.min.js
+
+mkdir -p $PRE_DEST/file-saver
+cp -u -f -R bower_components/file-saver/*min.js $PRE_DEST/file-saver/FileSaver.min.js
---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  staticFileGlobs: [
+    'build/bundled/index.html',
+    'build/bundled/shared/bundle_1.html',
+    'build/bundled/src/views/settings-view.html',
+    'build/bundled/bower_components/ace-builds/src-min-noconflict/**.*',
+    'build/bundled/bower_components/jszip/dist/jszip.min.js',
+    'build/bundled/bower_components/file-saver/FileSaver.min.js'
+  ],
+  stripPrefix: 'build/bundled/'
+};
